651ee27cdecd82243779be9b3a290e263c022cd8
Matthlrx22 <129058226+Matthlrx22@users.noreply.github.com>
2024-10-21 19:24:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function NavPre() {
+    window.location.href = 'Presentation.html';
+}
+
+function NavPar() {
+    window.location.href = 'Parcours.html';
+}
+
+function NavExp() {
+    window.location.href = 'Experience.html';
+}
